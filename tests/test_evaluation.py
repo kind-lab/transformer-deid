7cba8f3ea64b482fed09a88ef68f565b39672cee
Xi Huang <xibill.huang@mail.utoronto.ca>
2021-12-16 17:54:13 -0500

    Complete metric testing

--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -14,7 +14,7 @@ metric = load_metric(metric_dir)
 text_labels = ['O', 'AGE', 'CONTACT', 'DATE', 'ID', 'LOCATION', 'NAME', 'PROFESSION']
 
 
-def test_class_metrics():
+def test_individual_class_metrics():
     """
     Generate 10 test pairs randomly, compare calculated metrics to sklearn.metrics
     """
@@ -38,16 +38,16 @@ def test_class_metrics():
         assert math.isclose(comp_metrics[text_label]['number'], target_metrics[text_label]['support'], rel_tol=1e-6)
         assert math.isclose(comp_metrics[text_label]['precision'], target_metrics[text_label]['precision'], rel_tol=1e-6)
         assert math.isclose(comp_metrics[text_label]['recall'], target_metrics[text_label]['recall'], rel_tol=1e-6)
-    # TODO: validate overall accuracy, precision, recall, f1
 
-def test_binary_metrics():
+
+def test_individual_binary_metrics():
     """
     Generate 10 test pairs randomly, compare calculated binary metrics to sklearn.metrics
     """
     predictions = np.random.randint(0, high=len(text_labels) - 1, size=(10, 250))
     references = np.random.randint(1, high=len(text_labels) - 1, size=(10, 250))
 
-    binary_predictions = predictions.flatten() == 0  # TODO: should be 6?
+    binary_predictions = predictions.flatten() == 0
     binary_references = references.flatten() == 0
 
     comp_metrics = evaluation.compute_metrics(predictions=predictions, labels=references, label_list=text_labels, metric=metric, binary_evaluation=True)
@@ -58,4 +58,123 @@ def test_binary_metrics():
     assert math.isclose(comp_metrics['PHI']['precision'], target_metrics['False']['precision'], rel_tol=1e-6)
     assert math.isclose(comp_metrics['PHI']['recall'], target_metrics['False']['recall'], rel_tol=1e-6)
     assert math.isclose(comp_metrics['overall_accuracy'], target_metrics['accuracy'], rel_tol=1e-6)
-    # TODO: validate overall precision, recall, f1
+
+
+def test_overall_metrics():
+    """
+    Use specific pre-calculated test cases to validate overall metrics, considering the exclusion of 'O'.
+    Note: Accuracy not tested since it directly uses the metrics library.
+
+    Precision = TP / (TP + FP)
+    Recall = TP / (TP + FN)
+    F1 = TP / (TP + 0.5 * (FP + FN))
+    """
+    # Multi-class
+    predictions1 = np.array(
+        [
+            [0, 0, 0, 0, 0],
+            [3, 3, 2, 1, 1],
+            [3, 0, 2, 0, 1]
+            ]
+    )
+
+    references1 = np.array(
+        [
+            [0, 0, 1, 0, 0],
+            [2, 3, 2, 2, 1],
+            [3, 3, 2, 0, 1]
+            ]
+    ) 
+    
+    comp_metrics1 = evaluation.compute_metrics(predictions=predictions1, labels=references1, label_list=text_labels, metric=metric, binary_evaluation=False)
+    
+    # label |  TP  |  FP  |  FN
+    #   0   |  -   |  -   |  -
+    #   1   |  2   |  1   |  1
+    #   2   |  2   |  0   |  2
+    #   3   |  2   |  1   |  1
+    # total |  6   |  2   |  4 
+
+    TP1 = 6
+    FP1 = 2
+    FN1 = 4
+    precision1 = TP1 / (TP1 + FP1)
+    recall1 = TP1 / (TP1 + FN1)
+    f1_1 = TP1 / (TP1 + 0.5 * (FP1 + FN1))
+
+    assert math.isclose(comp_metrics1['overall_precision'], precision1, rel_tol=1e-6)
+    assert math.isclose(comp_metrics1['overall_recall'], recall1, rel_tol=1e-6)
+    assert math.isclose(comp_metrics1['overall_f1'], f1_1, rel_tol=1e-6)
+
+
+    # Multi-class
+    predictions2 = np.array(
+        [
+            [1, 3, 2, 3, 1],
+            [1, 3, 2, 1, 1],
+            [3, 0, 2, 0, 2]
+            ]
+    )
+
+    references2 = np.array(
+        [
+            [1, 3, 1, 3, 3],
+            [2, 3, 2, 2, 1],
+            [1, 3, 2, 0, 1]
+            ]
+    )
+
+    comp_metrics2 = evaluation.compute_metrics(predictions=predictions2, labels=references2, label_list=text_labels, metric=metric, binary_evaluation=False)
+    
+    # label |  TP  |  FP  |  FN
+    #   0   |  -   |  -   |  -
+    #   1   |  2   |  3   |  3
+    #   2   |  2   |  2   |  2
+    #   3   |  3   |  1   |  2
+    # total |  7   |  6   |  7
+
+    TP2 = 7
+    FP2 = 6
+    FN2 = 7
+    precision2 = TP2 / (TP2 + FP2)
+    recall2 = TP2 / (TP2 + FN2)
+    f1_2 = TP2 / (TP2 + 0.5 * (FP2 + FN2))
+
+    assert math.isclose(comp_metrics2['overall_precision'], precision2, rel_tol=1e-6)
+    assert math.isclose(comp_metrics2['overall_recall'], recall2, rel_tol=1e-6)
+    assert math.isclose(comp_metrics2['overall_f1'], f1_2, rel_tol=1e-6)
+
+
+    # Binary
+    predictions3 = np.array(
+        [
+            [1, 3, 5, 3, 6],
+            [5, 3, 2, 1, 1],
+            [5, 0, 2, 0, 7]
+            ]
+    )
+
+    references3 = np.array(
+        [
+            [0, 2, 2, 3, 3],
+            [0, 3, 0, 2, 0],
+            [5, 3, 3, 0, 0]
+            ]
+    )    
+    
+    comp_metrics3 = evaluation.compute_metrics(predictions=predictions3, labels=references3, label_list=text_labels, metric=metric, binary_evaluation=True)
+
+    # label |  TP  |  FP  |  FN
+    #   0   |  -   |  -   |  -
+    # non-0 |  8   |  5   |  1
+
+    TP3 = 8
+    FP3 = 5
+    FN3 = 1
+    precision3 = TP3 / (TP3 + FP3)
+    recall3 = TP3 / (TP3 + FN3)
+    f1_3 = TP3 / (TP3 + 0.5 * (FP3 + FN3))
+
+    assert math.isclose(comp_metrics3['overall_precision'], precision3, rel_tol=1e-6)
+    assert math.isclose(comp_metrics3['overall_recall'], recall3, rel_tol=1e-6)
+    assert math.isclose(comp_metrics3['overall_f1'], f1_3, rel_tol=1e-6)
