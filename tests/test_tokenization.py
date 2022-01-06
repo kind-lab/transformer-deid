0c4f0ab63f272f3e6a85f199320cf51e29b95b7a
Xi Huang <xibill.huang@mail.utoronto.ca>
2022-01-06 00:59:45 -0500

    Add unit test for split_sequences()

--- tests/test_tokenization.py
+++ tests/test_tokenization.py
@@ -36,3 +36,33 @@ def test_assign_tags(tokenizer):
     for n in name_tokens:
         idx = tokens.index(n)
         assert tags[idx] == 'NAME'
+
+
+def test_split_sequence(tokenizer):
+    """ Verify sequence splitting according to max sequence length is done correctly. """
+    text = 'Hello my name is Alistair Johnson and I do not have a pneumothorax.'
+    long_text = 'Hello my name is Alistair Johnson and I do not have a pneumothorax. ' * 100
+    split_long = tokenization.split_sequences(tokenizer, [long_text])
+    split_long2 = tokenization.split_sequences(tokenizer, [long_text, long_text])
+
+    # Case for short sentence
+    assert tokenization.split_sequences(tokenizer, [text]) == [text]
+
+    # Case for single test sentence
+    for split_text in split_long[:-2]:
+        assert len(tokenizer(split_text, add_special_tokens=False)['input_ids']) == tokenizer.max_len_single_sentence
+    assert len(tokenizer(split_long[:-1], add_special_tokens=False)['input_ids']) <= tokenizer.max_len_single_sentence
+
+    assert ''.join(split_long) == long_text
+
+    # Case for multiple test sentences
+    half = int(len(split_long2) / 2)
+    for split_text in split_long2[: half - 2]:
+        assert len(tokenizer(split_text, add_special_tokens=False)['input_ids']) == tokenizer.max_len_single_sentence
+    assert len(tokenizer(split_long2[half - 1], add_special_tokens=False)['input_ids']) <= tokenizer.max_len_single_sentence
+    
+    for split_text in split_long2[half: half - 2]:
+        assert len(tokenizer(split_text, add_special_tokens=False)['input_ids']) == tokenizer.max_len_single_sentence
+    assert len(tokenizer(split_long2[-1], add_special_tokens=False)['input_ids']) <= tokenizer.max_len_single_sentence
+
+    assert ''.join(split_long2) == long_text*2
