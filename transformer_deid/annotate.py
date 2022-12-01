ca05a263864d2d98f95dc9f0ed18804260dbc5bb
Callandra Moore <cmoore@qmed.ca>
2022-12-01 10:13:49 -0500

    fix annotation method

--- transformer_deid/annotate.py
+++ transformer_deid/annotate.py
@@ -3,56 +3,37 @@ import argparse
 import os
 import numpy as np
 from pathlib import Path
+import logging
+from tqdm import tqdm
 from data import DeidDataset
 from tokenization import convert_subtokens_to_label_list, merge_sequences
 from transformers import AutoModelForTokenClassification, AutoTokenizer
-from transformers import Trainer, TrainingArguments
-# from convert_to_label_list import convert_encodings_to_label_list, merge_sequences
 from load_data import load_data, create_deid_dataset
 from train import which_transformer_arch
 
+logger = logging.getLogger(__name__)
 
 def annotate(modelDir: str, test_dataset: DeidDataset):
     """Annotates dataset with PHI labels.
 
        Args:
             modelDir: directory containing config.json, pytorch_model.bin, and training_args.bin
-                e.g., 'i2b2_2014_{base architecture}_Model_{epochs}'
-            deid_task: DeidTask, see data.py
+                e.g., './transformer_models/{base architecture}_model_{epochs}'
             test_dataset: DeidDataset, see data.py
 
        Returns:
-            predicted_label:
-            labels:
+            new_labels: list of lists of [entity type, start, length] objects for each document
     """
-    # get parameters
-    epochs = int(modelDir.split('_')[-1])
-    out_dir = '/'.join(modelDir.split('/')[0:-1]) # this is a necessary parameter for some reason
-    train_batch_size = 8
-
     device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
     model = AutoModelForTokenClassification.from_pretrained(modelDir).to(device)
-
     model.eval()
 
-    training_args = TrainingArguments(
-        output_dir=out_dir,
-        num_train_epochs=epochs,
-        per_device_train_batch_size=train_batch_size,
-        per_device_eval_batch_size=8,
-        warmup_steps=500,
-        weight_decay=0.01,
-        logging_dir='./logs',
-        logging_steps=10,
-        save_strategy='no')
-
-    trainer = Trainer(model=model,
-                      args=training_args)
+    logger.info(f'Running predictions over {len(test_dataset.encodings.encodings)} split sequences from {len(test_dataset.ids)} documents.')
+    result = [get_logits(encoding, model, device) for encoding in tqdm(test_dataset.encodings.encodings, total=len(test_dataset.encodings.encodings))]
+    predicted_label = np.argmax(result, axis=2)
     
     id2label = model.config.id2label
-    predictions, labels, _ = trainer.predict(test_dataset)
-    predicted_label = np.argmax(predictions, axis=2)
     predicted_label = [[id2label[token] for token in doc] for doc in predicted_label]
 
     labels = []
@@ -61,9 +42,14 @@ def annotate(modelDir: str, test_dataset: DeidDataset):
 
     new_labels = merge_sequences(labels, test_dataset.ids)
 
-
     return new_labels
 
+def get_logits(encodings, model, device):
+    """ Return predicted labels from the encodings of a *single* text example. """
+    result = model(input_ids=torch.tensor([encodings.ids]).to(device),
+                attention_mask=torch.tensor([encodings.attention_mask]).to(device))
+    logits = result['logits'].cpu().detach().numpy()
+    return logits[0]
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -113,8 +99,7 @@ def main(args):
         raise NotImplementedError('sorry! can\'t save yet!')
     
     else:
-        print(annotations[0])
-        return annotations
+        return annotations, test_dataset.ids
 
 if __name__ == '__main__':
     args = parse_args()
